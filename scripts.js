5e85d58dc8cf8bba582c05400688652406d3a331
Thales Ruano <thalesoruano@gmail.com>
2021-11-04 13:52:44 -0300

    Construída a distribuição de cartas, adicionado reset.css

---
+++ scripts.js
@@ -0,0 +1,100 @@
+//LIsta de dados que armazena os passaros, como objetos
+const passaros = 
+[
+  {
+    id: 0,
+    identificador: "bobrossparrot",
+    imagem: "resources/bobrossparrot.gif"
+  },
+  {
+    id: 1,
+    identificador: "explodyparrot",
+    imagem: "resources/explodyparrot.gif"
+  },
+  {
+    id: 2,
+    identificador: "fiestaparrot",
+    imagem: "resources/fiestaparrot.gif"
+  },
+  {
+    id: 3,
+    identificador: "metalparrot",
+    imagem: "resources/metalparrot.gif"
+  },
+  {
+    id: 4,
+    identificador: "revertitparrot",
+    imagem: "resources/revertitparrot.gif"
+  },
+  {
+    id: 5,
+    identificador: "tripletsparrot",
+    imagem: "resources/tripletsparrot.gif"
+  },
+  {
+    id: 6,
+    identificador: "unicornparrot",
+    imagem: "resources/unicornparrot.gif"
+  }
+];
+
+//Iniciando as variáveis globais que serão acessadas nas funções.
+let arrayPares = [4, 6, 8, 10, 12, 14];
+let jogadas = 0;
+let numDeCartas = null;
+let paresRestantes = null;
+let container = null;
+let baralho = null;
+let escolhidos = null;
+
+//Função que faz o jogo iniciar.
+function Iniciar()
+{
+    do {
+          numDeCartas = parseInt(prompt('Com quantas cartas quer jogar? (Somente pares de 4 a 14)'));
+    }
+    while(!arrayPares.includes(numDeCartas));
+    paresRestantes = numDeCartas/2;
+    escolhidos = passaros.sort(comparador).slice(0,paresRestantes);
+    container = document.querySelector("section > .container");
+    baralho = container.querySelector("ul");
+    baralho.innerHTML = '';
+    container.className = '';
+    container.classList.add(`pair-of-${paresRestantes}`);
+    baralho.insertAdjacentHTML("afterbegin", distribuirCartas(escolhidos.sort(comparador)));
+    baralho.insertAdjacentHTML("afterbegin", distribuirCartas(escolhidos.sort(comparador)));
+}
+ 
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+  return Math.random() - 0.5; 
+}
+
+// Função que distrubui as cartas(preenche a lista ul com seus respectivos li)
+function distribuirCartas(lista) {
+  let html;
+
+  html = lista
+    .map((item) => {
+      return ` 
+        <li class="card" onClick="selecionar(this)" data-identificador="${item.identificador}">
+      <div class="front-face face">
+        <img src="resources/front.png" alt="">
+      </div>
+      <div class="back-face face">
+        <img src="${item.imagem}" alt="">
+      </div>
+    </li>
+         `;
+    })
+    .join("");
+
+  return html;
+}
+
+
+
+
+
+Iniciar();
